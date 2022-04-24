a853bd28a4c103e3769bf60c4d35d683fdad8d10
Andi <andy.u789@gmail.com>
2022-04-24 22:44:19 +0300

    bug fix

--- components/search-bar.tsx
+++ components/search-bar.tsx
@@ -17,8 +17,7 @@ const SearchBar = ({ setLoadedResult }: Props) => {
   const { autoPlay, SetAutoPlay } = usePlayerContext();
   const router = useRouter();
   const [searchValue, setSearchValue] = useState<string>(
-    (router.query.query as string)?.replace('%2520', ' ').replace('%20', ' ') ||
-      ''
+    (router.query.query as string).replaceAll('%20', ' ') || ''
   );
   const [loading, setLoading] = useState(false);
 
