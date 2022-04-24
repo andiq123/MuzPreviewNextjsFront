47521fed2456ed15c95afed598907decdca4b0b2
Andi <andy.u789@gmail.com>
2022-04-24 22:40:16 +0300

    da

--- components/search-bar.tsx
+++ components/search-bar.tsx
@@ -17,7 +17,7 @@ const SearchBar = ({ setLoadedResult }: Props) => {
   const { autoPlay, SetAutoPlay } = usePlayerContext();
   const router = useRouter();
   const [searchValue, setSearchValue] = useState<string>(
-    (router.query.query as string)?.replace('%20', ' ').replace('%2520', ' ') ||
+    (router.query.query as string)?.replace('%2520', ' ').replace('%20', ' ') ||
       ''
   );
   const [loading, setLoading] = useState(false);
